6b936d2c93f424546a9d49a2b12f696003ccf030
sonia.cabrera <sonia.cabrera@mh.gob.sv>
2020-09-14 16:56:09 -0600

    3

--- src/main/java/com/prueba/apache/kafka/config/KafkaConfig.java
+++ src/main/java/com/prueba/apache/kafka/config/KafkaConfig.java
@@ -2,13 +2,20 @@ package com.prueba.apache.kafka.config;
 
 import com.prueba.apache.kafka.mensajeDTO.ResultMsj;
 import com.prueba.apache.kafka.mensajeDTO.VehiculoMsj;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 /**
  *
@@ -17,6 +24,45 @@ import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 @Configuration
 public class KafkaConfig {
 
+     @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://172.17.0.8:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "vehiculo-result-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+    
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+            kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    public ConsumerFactory<String, VehiculoMsj> vehiculoConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"http://172.17.0.8:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "vehiculo-result-group");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                new JsonDeserializer<>(VehiculoMsj.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, VehiculoMsj> vehiculoKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, VehiculoMsj> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(vehiculoConsumerFactory());
+        return factory;
+    }
+    
     @Bean
     public ReplyingKafkaTemplate<String, VehiculoMsj, ResultMsj> replyingKafkaTemplate(ProducerFactory<String, VehiculoMsj> pf,
             ConcurrentKafkaListenerContainerFactory<String, ResultMsj> factory) {
